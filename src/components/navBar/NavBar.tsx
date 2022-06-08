e3123008ec3aa09089abfd59e39da3e33d695aaa
Diego Casanova <diego.ca@embluemail.com>
2022-06-08 20:52:15 -0300

    Cambios de estilo en navbar Accion clickeando en icono para volver atras

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -2,18 +2,22 @@ import React from 'react'
 import { AppBar, Box, IconButton, Toolbar } from '@mui/material'
 import mercadoLibreLogo from '../../assets/Logo_ML.png'
 import { SearchInput } from '../search-input/SearchInput'
+import { useNavigate } from 'react-router-dom'
 
 export const NavBar = () => {
+
+  const navigate = useNavigate()
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar sx={{ display:'flex', backgroundColor:'#FFE600'}}>
-        <Toolbar>
+      <AppBar position='static' sx={{ display:'flex', backgroundColor:'#FFE600'}}>
+        <Toolbar style={{ display:'flex', justifyContent:'center' }}>
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={ ()=> navigate('/')}
           >
             <img
                 src={`${mercadoLibreLogo}`}
